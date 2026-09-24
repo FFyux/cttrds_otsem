c9f4c8bf359de93de4811c39bc7847b8391772db
Florent Fayeaux <f.fayeaux@saint-etiennetourisme.com>
2026-09-24 15:32:44 +0200

    Ajouter les tests et la CI Django

--- suivi/tests.py
+++ suivi/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+#from django.test import TestCase
+from django.conf import settings
+from django.test import SimpleTestCase
 
+
+class ConfigurationSmokeTests(SimpleTestCase):
+    def test_secret_key_is_configured(self):
+        self.assertTrue(settings.SECRET_KEY)
+
+    def test_secret_key_is_not_the_old_default(self):
+        self.assertFalse(settings.SECRET_KEY.startswith("django-insecure"))
 # Create your tests here.
